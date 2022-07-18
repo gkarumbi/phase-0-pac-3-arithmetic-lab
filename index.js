8a0d2e6ef5185cc30c7eb03d0a112814ca4142ea
George Karumbi <gkarumbi@gmail.com>
2022-07-18 21:12:00 +0300

    Completed assignment

---
+++ index.js
@@ -0,0 +1,53 @@
+let number = 5;
+
+number++;
+
+number;
+
+number --;
+
+number;
+
+
+--number;
+
+number;
+
+++number
+
+
+number
+
+number += 3
+
+number -=2
+
+number *=10
+
+
+
+number /=5
+
+
+number = 10;
+
+function add5(){
+    return (number+=5);
+}
+
+
+function divideBy3(){
+    return (number/=3)
+
+}
+
+divideBy3();
+
+add5();
+
+
+number=10;
+
+add5();
+
+divideBy3();
